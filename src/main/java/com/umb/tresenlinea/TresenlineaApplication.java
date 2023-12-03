b4c31c391fbe1a91434d03e68d26bb82ef1db981
fayalac <fayalaca@emeal.nttdata.com>
2023-12-03 01:44:19 -0500

    Configuracion heroku

--- src/main/java/com/umb/tresenlinea/TresenlineaApplication.java
+++ src/main/java/com/umb/tresenlinea/TresenlineaApplication.java
@@ -7,6 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TresenlineaApplication {
 
 	public static void main(String[] args) {
+		// Obtén el puerto del entorno de Heroku o usa un valor predeterminado
+		String port = System.getenv("PORT");
+		if (port == null) {
+			port = "8080"; // Puerto predeterminado si no se encuentra la variable PORT
+		}
+
+		// Configura el puerto
+		System.setProperty("server.port", port);
+
+		// Inicia la aplicación
 		SpringApplication.run(TresenlineaApplication.class, args);
 	}
 
